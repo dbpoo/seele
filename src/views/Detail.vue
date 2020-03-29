900bd754ab114d9f08ba6345dc45e515b63e88ff
17074795 <17074795@suning.com>
2020-03-29 19:25:28 +0800

    新闻详情页新增

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,7 +1,108 @@
-<template> </template>
+<template>
+  <div class="container">
+    <div class="banner"></div>
+    <div class="detail" v-if="newsCon">
+      <div class="section">
+        <div class="infos">
+          <div class="location">新闻动态 &gt; <span>新闻详情</span></div>
+          <div class="time">{{ newsDate }}</div>
+        </div>
+        <div class="title">{{ newsTit }}</div>
+        <div class="content" v-html="newsCon"></div>
+      </div>
+    </div>
+    <div class="loading" v-if="isLoading">
+      <div class="ball-pulse">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "detail",
+  data() {
+    return {
+      id: this.$route.params.id,
+      newsTit: "",
+      newsCon: "",
+      newsDate: "",
+      isLoading: true
+    };
+  },
+  methods: {
+    getCms() {
+      this.$aixos
+        .get("http://api.lkbt.pro/wp-json/wp/v2/posts/" + this.id)
+        .then(res => {
+          this.newsTit = res.data.title.rendered;
+          this.newsCon = res.data.content.rendered;
+          this.newsDate = this._filterTime(res.data.date);
+          this.isLoading = false;
+        });
+    },
+    _filterTime(t) {
+      return t.split("T")[0];
+    }
+  },
+  mounted() {
+    this.getCms();
+  }
+};
 </script>
 
-<style></style>
+<style scoped lang="less">
+.banner {
+  height: 581px;
+  background: url("../assets/banner_news.jpg") center 0 no-repeat;
+}
+.detail {
+  overflow: hidden;
+  padding-top: 132px;
+  background-color: #fff;
+  color: #6c6c6c;
+  .infos {
+    display: flex;
+    font-size: 20px;
+    justify-content: space-between;
+    padding: 8px 0;
+    border-bottom: 1px solid #007586;
+    span {
+      color: #007586;
+    }
+  }
+  .title {
+    margin-top: 80px;
+    margin-bottom: 65px;
+    font-size: 36px;
+    text-align: center;
+    color: #484848;
+  }
+  .content {
+    padding: 0 20px;
+    margin-bottom: 160px;
+    /deep/ p {
+      color: #6c6c6c;
+      text-indent: 2em;
+      font-size: 24px;
+      line-height: 50px;
+      text-align: justify;
+    }
+  }
+  .source {
+    margin: 40px 0 160px 0;
+    text-align: right;
+    font-size: 20px;
+  }
+}
+
+.loading {
+  min-height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
